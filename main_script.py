a2f355ca60763fc53cca4de20c4356a1c3f3bf2e
Garsdal <garsdal@live.dk>
2017-11-23 15:59:34 +0100

    hej

--- main_script.py
+++ main_script.py
@@ -7,10 +7,46 @@ Created on Thu Nov 23 15:14:11 2017
 
 from grade_rounding_function import *
 
+
+
 while(True):
     
     print("Welcome to our grading calculator\n")
+    print("Input the name of your .csv file to start the program")
+    print("Type 'quit' if you wish to quit")
     
-    filename = input("name of your csv file: \n")
+    while(True):
+    
+    option = input("Input: \n")
+    
+        if (option == "hej"):
+            print("lol")
+            
+        elif (option == 'quit') or (option == 'q'):
+            break    
+        
+        print("sup")
+        print("1. test1\n2. test2\n3. test3\n3. test3\n4. test4\n5. quit")
+            
+        choice = input("What would you like to see?\n")
+            
+        if (choice == "1"):
+            print("bruh")
+            
+        elif (choice == "2"):
+            print("bruh")
+            
+        elif (choice == "3"):
+            print("bruh")
+            
+        elif (choice == "4"):
+            print("bruh")
+            
+        elif (choice == "5") or (choice =="quit") or (choice =="q"):
+            break
+            
+    
+    
+    
     
     

bb14f7c48cce9d2f7e26e163533e4f3fbade250f
emilballermann <33318888+emilballermann@users.noreply.github.com>
2017-11-23 13:51:59 +0100

    Test if shit works

---
+++ functions.py
@@ -0,0 +1,12 @@
+''' Program for grading students
+==========================================================
+
+Project start: 23rd of November 2017
+
+Project by Adam Beilin, Marcus Garsdal and Emil Ballermann'''
+
+
+
+Hej Adam og Marcus
+
+Skipperlabskovs

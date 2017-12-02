b1cc90218bbaf9a8b693b7843d97e0e9f473164f
emilballermann <33318888+emilballermann@users.noreply.github.com>
2017-12-02 21:10:49 +0100

    Plots done (need checking)

--- gradesPlotFunction.py
+++ gradesPlotFunction.py
@@ -10,24 +10,19 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from FinalGradeFunction import *
-
+a=[4,7,12,10,10,00,2]
+b=[12,10,7,4,4,2,2,]
+c=[00,-3,00,-3,-3,4,4]
+d=[4,7,7,10,00,2,2]
+grades = np.vstack([a,b,c,d])
+data=np.copy(grades)
 
 #Defines plot function
 def gradesPlot(grades):
     '''
-    INPUT:
-        data: An N x M matrix
+=============Plot "Final grades"===============
 
-    OUTPUT:
-        A bar plot of the number of students who have recieved each of possible 
-        final grades on the 7-step-scale (computed using the function 
-        computeFinalGrades)
-
-    USAGE:
-        gradesPlot(grades)
-    '''
-    
-        #Plot "Final grades"
+    '''        
     xAxis = [1,2,3,4,5,6,7]
     
     
@@ -46,18 +41,15 @@ def gradesPlot(grades):
    
     plt.figure(1,figsize=(6,6)) #Set first figure with 1:1 ratio
     plt.subplots_adjust(top=0.88, bottom=0.225, left=0.11, right=0.9,
-                        hspace=0.2,wspace=0.2) #Adjust size
-
+                        hspace=0.2,wspace=0.2) #Adjusting size
+    #Plotting bar plot
     for i in range(0,len(xAxis)):
         plt.bar(xAxis[i],yAxis[i],color=next(prop_iter)['color'], alpha=1) #Plot bar plot with colors
-        
-    
+
     plt.xticks(xAxis,bacStr, rotation = 35) #Set lables and rotation xAxis,bacStr,rotation=35
     plt.title("Final grades") #Set title
     plt.xlabel("Grades") #Assign name to x-axis
     plt.ylabel("Number of students") #Assign name to y-axis
-    
-  
     
     #Making the y-axis only integers
     locs, labels = plt.yticks()
@@ -67,38 +59,38 @@ def gradesPlot(grades):
 
     plt.show()
     
-"====================================="
+    '''
+===============Plot "Grades per assignment"==================
+    '''
+               
+    #Save the number of students and assignments as variables
+    N = matrixShape(grades)[0]
+    M = matrixShape(grades)[1]
+    y = [-3,00,2,4,7,10,12]
+    #Making a matrix with random numbers for dot-position
+    x=np.random.uniform(-0.1,0.1,M*N).reshape(N,M)
+    newdata = data + x
+    #Make dots for each assignment
+    for i in range(M):
+        plt.plot(np.random.uniform(-0.1,0.1,N)+i+1, newdata[:,i] ,color='blue',marker='o',linestyle='None', markersize=5)
+    #Plot the mean grade for each assignment as a line  
+    mean=np.ones(M)
+    for i in range(M):
+        mean[i]=np.mean(newdata[:,i])
+    plt.plot(np.arange(M)+1, mean ,color='red',linestyle='-',label="Mean grade")
 
-     #Plot "Grades per assignment"
-
-#Defining length of datamatrix
-x = np.array(range(len(grades)))
-
-xLabels=[""]*len(grades)
-
-for i in range(len(grades)):
-    xLabels[i]="Assignment no. "+" "+str(i+1)
-print(xLabels)
-
-y = [-3,00,2,4,7,10,12]
-yLabels = [-3,00,2,4,7,10,12]
-
-plt.plot(x,y)
-
-
-plt.xticks(x, xLabel, rotation = 35)
-plt.yticks(y,)
-plt.show()
-
+    plt.title("Grades per assignment") # Set the title of the graph
+    plt.yticks(y, rotation = 35)
+    plt.xlabel("Assignment number") # Set the x-axis label 
+    plt.ylabel("Grade") # Set the y-axis label 
+    plt.grid(True) #Attach grid to plot
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
+    
+    #define empty variable (y) to return from function
+    y=" "
+    return y
+    
 
 print(gradesPlot(grades))
-    
-'''“Grades per assignment”: A plot with the assignments on the x-axis and the grades on the y-axis. The x-axis must
- show all assignments from 1 to M, and the y-axis must show all grade −3 to 12. The plot must contain: 
-     1. Each of the given grades marked by a dot. You must add a small random number (between -0.1 and 0.1) to the x- and
- y-coordinates of each dot, to be able tell apart the diﬀerent dots which otherwise would be on top of each other 
- when more than one student has received the same grade in the same assignment. 
-     2. The average grade of each of 
- the assignments plotted as a line
-The plots should include a suitable title, descriptive axis labels, and a data legend where appropriate. You are
- allowed to present the plots in separate ﬁgure windows or as sub-plots in a single ﬁgure window.'''
+
